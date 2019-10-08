c9209fefd9d8f0f9e930b086806268bf11ff8acf
Sezer Tunca <sezertunca@gmail.com>
2019-10-08 21:00:07 +0100

    Initial Commit

---
+++ AutoLayoutInCode/AutoLayoutInCode.h
@@ -0,0 +1,19 @@
+//
+//  AutoLayoutInCode.h
+//  AutoLayoutInCode
+//
+//  Created by Sezer Tunca on 08/10/2019.
+//  Copyright © 2019 Sezer Tunca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoLayoutInCode.
+FOUNDATION_EXPORT double AutoLayoutInCodeVersionNumber;
+
+//! Project version string for AutoLayoutInCode.
+FOUNDATION_EXPORT const unsigned char AutoLayoutInCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLayoutInCode/PublicHeader.h>
+
+
